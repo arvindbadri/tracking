b87093f5f4c50c601693c1f23287668e366580dc
Badrinath <abadrinath@paypalcorp.com>
2016-08-11 17:45:29 +0530

    made stuff simpler

--- Block.java
+++ Block.java
@@ -1,5 +1,11 @@
 public class Block {
     int start;
     int end;
-    Identifier identifier;
+    String identifier;
+
+    public Block(int start, int end, String identifier) {
+        this.start = start;
+        this.end = end;
+        this.identifier = identifier;
+    }
 }
--- Orchestrator.java
+++ Orchestrator.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Orchestrator {
     List<Block> blocks;
+    public Orchestrator() {
+        blocks = new ArrayList<>();
+    }
+    private void addBlock(Block block) {
+        resolveConflictsAndAdd(block);
+        reduce();
+    }
+    public void addBlock(String line) {
+        Scanner sc = new Scanner(line);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        String identifier = sc.next();
+    }
 }
